a4b5fdccb6339df12a9d9b06a08652b4999af85e
JGarnick <garnick.josh@gmail.com>
2017-04-13 17:43:48 -0700

    Adding project and extras to repo

---
+++ HelloAndroid/HelloAndroid.Xml/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloAndroid.Xml
+{
+    [Activity(Label = "HelloAndroid.Xml", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            //var layout = new LinearLayout(this);
+            //layout.Orientation = Orientation.Vertical;
+            //var aLabel = new TextView(this);
+            //aLabel.Text = "Hello, Xamarin.Android";
+            //var aButton = new Button(this);
+            //aButton.Text = "Say Hello";
+            //aButton.Click += (sender, e) => {
+            //    aLabel.Text = "Hello from the button";
+            //};
+
+            //layout.AddView(aLabel); layout.AddView(aButton);
+            //SetContentView(layout);
+
+            SetContentView(Resource.Layout.Main);
+            
+            var aButton = FindViewById<Button>(Resource.Id.aButton);
+            var aLabel = FindViewById<TextView>(Resource.Id.helloLabel);
+            var aResetButton = FindViewById<Button>(Resource.Id.aResetButton);
+            aButton.Click += (sender, e) => {
+                aLabel.Text = "Hello from the button";
+            };
+            aResetButton.Click += (sender, e) => {
+                aLabel.Text = "Hello Xamarin.Android";
+            };
+        }
+    }
+}
+
---
+++ HelloAndroid/HelloAndroid/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloAndroid
+{
+    [Activity(Label = "HelloAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            //Create the user interface in code
+            string defaultText = "Hello, Xamarin.Android";
+            var layout = new LinearLayout(this);
+            layout.Orientation = Orientation.Vertical;
+            var aLabel = new TextView(this);
+            aLabel.Text = defaultText;
+            var aButton = new Button(this);
+            aButton.Text = "Say Hello";
+            var aResetButton = new Button(this);
+            aResetButton.Text = "Reset";
+            aButton.Click += (sender, e) =>
+            {
+                aLabel.Text = "Hello from the button";
+            };
+
+            aResetButton.Click += (sender, e) =>
+            {
+                aLabel.Text = defaultText;
+            };
+
+            layout.AddView(aLabel);
+            layout.AddView(aButton);
+            layout.AddView(aResetButton);
+            SetContentView(layout);
+        }
+
+    }
+}
+
